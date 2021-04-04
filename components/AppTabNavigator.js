18ef5c58a99181e130e417efa1fb7309a5739ee6
SanjananSantosh <71774680+SanjananSantosh@users.noreply.github.com>
2021-04-04 19:20:34 +0530

    Add files via upload

---
+++ components/AppTabNavigator.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Image } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { AppStackNavigator } from './AppStackNavigator'
+import homeScreen from '../screens/homeScreen';
+import setupScreen from '../screens/setupScreen';
+
+
+export const AppTabNavigator = createBottomTabNavigator({
+  home : {
+    screen: homeScreen,
+    navigationOptions :{
+      tabBarIcon : <Image source={require("../assets/request-list.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "Donate Books",
+    }
+  },
+  setup: {
+    screen: setupScreen,
+    navigationOptions :{
+      tabBarIcon : <Image source={require("../assets/request-book.png")} style={{width:20, height:20}}/>,
+      tabBarLabel : "Book Request",
+    }
+  }
+});
